279d2e066a2646a2b8bf0674b21715bb5e7c4253
Ryan_Thomas <rthoma11@villanova.edu>
2016-11-30 15:23:01 -0800

    sing hw

--- starter-code/sing.js
+++ starter-code/sing.js
@@ -1,1 +1,5 @@
 console.log("sing.js loaded");
+
+for (var beer = 5; beer > 0; beer--) {
+  console.log([i] + " bottles of beer on the wall, " + [i] " bottles of beer! Take one down and pass it around, " + [i-1] + " bottles of beer on the wall!")
+}
